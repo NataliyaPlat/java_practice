3cea6c842e0e05f9f2d9c1894089e2cb6bfb9d97
NataliyaPlat <60834307+NataliyaPlat@users.noreply.github.com>
2020-02-13 20:03:06 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-    System.out.println("hello world");
+       // Create a loop that will print numbers from 1 to 10, except number 7
+    for (int num=1; num<=10; num++){
+        if (num==7){
+            continue;
+        }
+        System.out.println(num);
+    }
+    //Create An Array of integers from 1 to 5, Call an integer on array using index and print it.
+        int[] num = {1,2,3,4,5};
+    for (int i = 0; i < num.length; i++){
+        System.out.println(num[i]);
+    }
+    //Call index outside of the bound. Handle this exception printing “Outside of bound message.”
+           try {
+               int[] myNumbers = {1, 2, 3, 4};
+               System.out.println(myNumbers[10]);
+           }
+            catch (Exception e){
+               System.out.println ("Outside of the bound");
+
+        }
+
     }
 }
