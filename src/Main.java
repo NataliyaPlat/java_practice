3b28c3ebe8b3f1f2583bf720654dc18f957432e5
NataliyaPlat <60834307+NataliyaPlat@users.noreply.github.com>
2020-02-13 22:36:29 -0600

    homework2

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,19 @@
+
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-       // Create a loop that will print numbers from 1 to 10, except number 7
-    for (int num=1; num<=10; num++){
-        if (num==7){
-            continue;
-        }
-        System.out.println(num);
-    }
-    //Create An Array of integers from 1 to 5, Call an integer on array using index and print it.
-        int[] num = {1,2,3,4,5};
-    for (int i = 0; i < num.length; i++){
-        System.out.println(num[i]);
-    }
-    //Call index outside of the bound. Handle this exception printing “Outside of bound message.”
-           try {
-               int[] myNumbers = {1, 2, 3, 4};
-               System.out.println(myNumbers[10]);
-           }
-            catch (Exception e){
-               System.out.println ("Outside of the bound");
-
-        }
-
+//You need to create a program, which will accept a string and check if total count on characters within the string is even it should return:
+//
+//“String is even” If not it should return: “String is not even”
+           String text;
+           System.out.println("Enter string:");
+           Scanner input = new Scanner(System.in);
+           text = input.nextLine();
+           int a=text.length();
+           if (a % 2 == 0)
+               System.out.println("Entered number is even");
+           else
+               System.out.println("entered number is odd");
     }
 }
